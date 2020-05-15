c7fdef021350c4f33d745033414f9205b60dec74
exactmike <mike@campbellnetwork.net>
2020-05-15 14:09:02 -0400

    updated setup with mount option & updatepassword

--- Setup.ps1
+++ Setup.ps1
@@ -1,25 +1,30 @@
 [cmdletbinding()]
 param(
-    [string]$ImageName = '2019-GA-ubuntu-16.04'
+    [string]$ImageName = '2019-latest'
     ,
     [SecureString]$InitialPassword
     ,
     [string]$ContainerName
     ,
     [SecureString]$ProductionPassword
+    ,
+    [switch]$UpdatePassword
 )
-$PlainInitialPassword = $InitialPassword | Convert-SecureStringToString
-$PlainProductionPassword = $ProductionPassword | Convert-SecureStringToString
+$PlainInitialPassword = $InitialPassword | ConvertFrom-SecureString -AsPlainText
+$PlainProductionPassword = $ProductionPassword | ConvertFrom-SecureString -AsPlainText
 
-docker pull mcr.microsoft.com/mssql/server:$ImageName
+if ($true -ne $UpdatePassword)
+{
+    docker pull mcr.microsoft.com/mssql/server:$ImageName
 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$PlainInitialPassword" `
-    -p 1433:1433 --name $ContainerName `
-    -v sqlvolume:/var/opt/mssql `
-    -d mcr.microsoft.com/mssql/server:$ImageName
+    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$PlainInitialPassword" `
+        -p 1433:1433 --name $ContainerName `
+        --mount 'type=volume,source=sqlvolume,destination=/var/opt/mssql' `
+        -d mcr.microsoft.com/mssql/server:$ImageName
 
-Write-Verbose -Message 'Sleeping for 10'
-Start-Sleep -Seconds 10
+    Write-Verbose -Message 'Sleeping for 10'
+    Start-Sleep -Seconds 10
+}
 
 docker exec -it $ContainerName /opt/mssql-tools/bin/sqlcmd `
     -S localhost -U SA -P $PlainInitialPassword `
